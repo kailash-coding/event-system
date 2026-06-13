6ec441c4445d89fa793d7dd964e1030caf40d1ee
Kailash suthar <kailashsuthar0723@gmail.com>
2026-06-13 22:34:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       },
       '/uploads': {
         target: 'http://localhost:5050',
-        changeOrigin: true,
+        changeOrigin: true, 
       },
     },
   },
